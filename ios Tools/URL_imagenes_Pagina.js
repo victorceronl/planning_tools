a84f7aa3ef7dcb117504a51b13ca521211f6002b
victorceronl <ceronloezavictor@gmail.com>
2025-09-22 14:43:40 -0600

    Modificación importante a obtención de Imagenes

--- ios Tools/URL_imagenes_Pagina.js
+++ ios Tools/URL_imagenes_Pagina.js
@@ -5,12 +5,9 @@
     .filter(url => url && url.startsWith("http")); // filtra URLs válidas
 
   if (imgs.length === 0) {
-    return "No se encontraron imágenes en esta página.";
+    completion("No se encontraron imágenes en esta página.");
+  } else {
+    // Devuelve las URLs separadas por salto de línea
+    completion(imgs.join("\n"));
   }
-
-  // Devuelve las URLs separadas por salto de línea para que Shortcuts las use
-  return imgs.join("\n");
 })();
-
-
-//Genera un listado de URL directo de cada una de las imagenes de una pagina Web
