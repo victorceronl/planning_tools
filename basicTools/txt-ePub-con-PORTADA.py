49f6c5e434b4300457e7b2238e96c9dd222f0334
victorceronl <ceronloezavictor@gmail.com>
2025-10-15 09:18:30 -0600

    Claridad

--- basicTools/txt-ePub-con-PORTADA.py
+++ basicTools/txt-ePub-con-PORTADA.py
@@ -49,8 +49,8 @@ def txt_a_epub(ruta_txt, ruta_epub, titulo="Mi Libro", autor="Desconocido", port
 if __name__ == "__main__":
     txt_a_epub(
         "mi_texto.txt",        # archivo de entrada
-        "Crear o Morir.epub",       # archivo de salida
-        titulo="Crear o Morir",
-        autor="Andres Oppenheimer",
+        "Deep Work.epub",       # archivo de salida
+        titulo="Deep Work",
+        autor="Cal Newport",
         portada="portada.jpg"  # imagen de portada opcional
     )
